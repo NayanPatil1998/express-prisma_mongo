db17c7afd9c09975ec744e246be99e061e99654d
Nayan Patil <patil.nayan04@gmail.com>
2022-08-21 20:25:09 +0530

    login and logout endpoint added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,8 @@
-import { hash } from "argon2";
+import { hash, verify } from "argon2";
 import { Request, Response } from "express";
 import { cookieToken } from "../utis/cookieToken";
 import prisma from "../../prisma/";
+import validator from "validator";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -12,24 +13,77 @@ export const register = async (req: Request, res: Response) => {
       });
     }
 
+    const hitUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (hitUser) {
+      return res.status(501).json({
+        message: `A user with email ${email} already exist`,
+      });
+    }
+
     const hashedPassword = await hash(password);
 
-    
     const user = await prisma.user.create({
-        data: {
-            email,
-            name,
-            password: hashedPassword,
-        },
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
     });
-    
-    console.log("user ", user);
-    cookieToken(user, res)
 
+    cookieToken(user, res);
   } catch (error) {
-    console.log(error)
-     res.status(501).json({
-        message: error.message,
+    console.log(error);
+    res.status(501).json({
+      message: error.message,
+    });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    if (
+      validator.isEmpty(email) ||
+      !validator.isEmail(email) ||
+      validator.isEmpty(password)
+    ) {
+      return res.status(501).json({
+        message: "Please provide correct fields",
       });
+    }
+
+    const user = await prisma.user.findUniqueOrThrow({ where: { email } });
+
+    const isPasswordCorrect = await verify(user.password, password);
+
+    if (!isPasswordCorrect) {
+      return res.status(501).json({
+        message: "Email or password fields is incorrect",
+      });
+    }
+    cookieToken(user, res);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const logout = async (req: Request, res: Response) => {
+  try {
+    res.clearCookie("token");
+    res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { register } from "../controllers/user.controller";
+import { login, logout, register } from "../controllers/user.controller";
 
 const userRouter = Router();
 
 userRouter.post('/register', register)
+userRouter.post('/login', login)
+userRouter.get('/logout', logout)
 
 
 export default userRouter
